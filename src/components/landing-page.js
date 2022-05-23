7520170ab5713b32ad4d14a693f7186820ffbb23
jgandrade <johnglenn.andrade@gmail.com>
2022-05-23 18:44:28 +0800

    Added Landing PageSample

---
+++ src/components/landing-page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function LandingPage() {
+    return(
+        <>
+            <div>
+                <h1>Welcome to Nomadph!</h1>
+            </div>
+        </>
+    );
+}
+
+export default LandingPage;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Favorites from './favorites';
-import Navbar from './navbar';
-import Footer from './footer';
+import Favorites from './components/favorites';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import LandingPage from './components/landing-page';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
